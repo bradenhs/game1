ae49e88dfd2b1128767be2a1490eda2dc48f9716
bradenhs (via zilch) <>
2023-04-06 09:03:56 -0400

    Initial Release

--- src/renderer.ts
+++
@@ -1,9 +0,0 @@
-
-import type { Dimensions } from "zilch-game-engine";
-import type { Config } from "./config";
-import type { State } from "./state";
-
-export class Renderer {
-  constructor(canvas: HTMLCanvasElement, config: Config) {}
-  render(state: State, dimensions: Dimensions) {}
-}
