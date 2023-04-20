44b8c1967c2615cb0c6e12c164d1c12df213beaa
Braden Snell <bradenhs@outlook.com>
2023-04-20 15:06:52 -0400

    done

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,20 @@
-
-import type { Dimensions } from "zilch-game-engine";
-import type { Config } from "./config";
+import type { RenderParams } from "zilch-game-engine";
 import type { State } from "./state";
+import type { Config } from "./config";
 
 export class Renderer {
-  constructor(canvas: HTMLCanvasElement, config: Config) {}
-  render(state: State, dimensions: Dimensions) {}
+  container: HTMLPreElement;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.container = document.createElement("pre");
+    this.container.style.position = "absolute";
+    this.container.style.display = "flex";
+    this.container.style.alignItems = "center";
+    this.container.style.justifyContent = "center";
+    this.container.style.fontFamily = "monospace";
+    document.body.appendChild(this.container);
+  }
+  render(params: RenderParams<State, Config>) {
+    this.container.innerText = JSON.stringify(params, null, 2);
+  }
 }
